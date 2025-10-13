0ddaf266335b90cfceca1f255aa9f4e4d87df90e
Igor Gonçalves Antunes <101950802+IgorAntunes66@users.noreply.github.com>
2025-10-13 20:00:37 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Isadora, encontrei um bug em mim: não consigo parar de pensar em você. Se me ama, me chama de "meu programador".</h1>
+  return (
+    <h1>
+      Isadora, encontrei um bug em mim: não consigo parar de pensar em você. Se
+      me ama, me chama de "meu programador".
+    </h1>
+  );
 }
 
 export default Home;
