d1e2734f44c92aac8b014300073ffd54d6b85f94
IgorAntunes66 <igorgantunes@hotmail.com>
2025-10-25 14:17:03 -0400

    add  endpoint

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
-const nextJest = require('next/jest')
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
 
-const createJestConfig = nextJest();
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
-})
+});
 
-module.exports = jestConfig
+module.exports = jestConfig;
