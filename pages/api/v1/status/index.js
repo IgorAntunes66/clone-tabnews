d829b89aab68a70aaa0822cadd854194294046dc
IgorAntunes66 <igorgantunes@hotmail.com>
2026-04-22 16:35:24 -0400

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,6 @@
-function status(request, response) {
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
     response.status(200).json({ chave: "Alunos do curso.dev são pessoas acima da média" });
 }
 
