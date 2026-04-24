ef73a9e070ad75debb3f4b8fc5820bf4fe5fa93c
IgorAntunes66 <igorgantunes@hotmail.com>
2026-04-24 17:26:14 -0400

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,32 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js"
+
 
 async function status(request, response) {
-    response.status(200).json({ chave: "Alunos do curso.dev são pessoas acima da média" });
+    const updatedAt = new Date().toISOString();
+
+    const dataBaseVersionResult = await database.query("SHOW server_version;")
+    const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version
+
+    const databaseMaxConnectionsResults = await database.query("SHOW max_connections;")
+    const databaseMaxConnectionsValue = databaseMaxConnectionsResults.rows[0].max_connections
+
+    const databaseName = process.env.POSTGRES_DB
+    const databaseOpenedConnectionsResult = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName]
+    });
+    const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count
+
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: dataBaseVersionValue,
+                max_connections: parseInt(databaseMaxConnectionsValue),
+                opened_connections: databaseOpenedConnectionsValue
+            }
+        }
+    });
 }
 
 export default status;
