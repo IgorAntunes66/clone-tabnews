9e55f075d5ce2e86717d0c0c571f6850f42d6500
IgorAntunes66 <igorgantunes@hotmail.com>
2026-07-16 11:10:48 -0400

    fix `eslint` linting

--- infra/database.js
+++ infra/database.js
@@ -1,43 +1,45 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
-    let client;
-    try {
-        client = await getNewClient();
-        const result = await client.query(queryObject)
-        return result;
-    } catch (error) {
-        console.error(error)
-        throw error;
-    } finally {
-        await client.end()
-    }
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 async function getNewClient() {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        ssl: getSSLValues()
-    });
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    ssl: getSSLValues(),
+  });
 
-    await client.connect();
-    return client;
+  await client.connect();
+  return client;
 }
 
-export default {
-    query,
-    getNewClient,
-}
+const database = {
+  query,
+  getNewClient,
+};
+
+export default database;
 
 function getSSLValues() {
-    if (process.env.POSTGRES_CA) {
-        return {
-            ca: process.env.POSTGRES_CA
-        }
-    }
-    return process.env.NODE_ENV === "production" ? true : false
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "production" ? true : false;
 }
