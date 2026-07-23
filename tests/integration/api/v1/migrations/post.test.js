bf244b5e13b2fdf0bda530fed6bf49c984543360
IgorAntunes66 <igorgantunes@hotmail.com>
2026-07-23 17:01:50 -0400

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -3,37 +3,43 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
 });
 
-beforeAll(cleanDatabase); //Função beforeAll serve para fazer algo antes de tudo, nesse caso, limpar o DB.
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
 
-async function cleanDatabase() {
-  //Função para limpar o DB e o teste não ficar intermitente
-  await database.query("drop schema public cascade; create schema public;");
-}
+        const responseBody = await response1.json();
 
-// Realizamos 2 request no endpoint para cobrir o caso onde a a migração de fato é executada...
-// E no outro é onde a migração ja foi executada no primeiro request, logo no segundo não tem nenhuma migração pendente.
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+        expect(Array.isArray(responseBody)).toBe(true);
+
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const responseBody2 = await response2.json();
+
+        expect(Array.isArray(responseBody2)).toBe(true);
+
+        expect(responseBody2.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const responseBody = await response1.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const responseBody2 = await response2.json();
-
-  expect(Array.isArray(responseBody2)).toBe(true);
-
-  expect(responseBody2.length).toBe(0);
 });
